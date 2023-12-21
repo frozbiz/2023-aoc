7ff3a5ee9cec8748f0a2db1a2ffa62c6cf2e0b8c
Lee K-A <lee.keyser-allen@sonos.com>
2023-12-21 14:31:35 -0500

    Set up structure and ran through day 2.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,96 @@
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
+
 fun main() {
+
+    fun String.toNumber(): Int{
+        return first { it.isDigit() }.digitToInt() * 10 + last { it.isDigit() }.digitToInt()
+    }
+
+    val numbers = listOf(
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    )
+    fun String.value(): Int {
+        return when (this) {
+            "0", "zero" -> 0
+            "1", "one" -> 1
+            "2", "two" -> 2
+            "3", "three" -> 3
+            "4", "four" -> 4
+            "5", "five" -> 5
+            "6", "six" -> 6
+            "7", "seven" -> 7
+            "8", "eight" -> 8
+            "9", "nine" -> 9
+            else -> throw IllegalArgumentException()
+        }
+    }
+    fun String.toNumber2(): Int{
+        val firstDigit = findAnyOf(numbers)?.second?.value() ?: throw IllegalStateException()
+        val lastDigit = findLastAnyOf(numbers)?.second?.value() ?: throw IllegalStateException()
+        return firstDigit * 10 + lastDigit
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (value in input) {
+            val num = value.toNumber()
+//            num.println()
+            sum += num
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (value in input) {
+            val num = value.toNumber2()
+//            num.println()
+            sum += num
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent().split("\\s+".toRegex())
+
+    check(part1(testInput) == 142)
+
+    val testInputTwo = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """.trimIndent().split("\\s+".toRegex())
+
+    check(part2(testInputTwo) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
---
+++ src/Day02.kt
@@ -0,0 +1,98 @@
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
+import java.lang.Integer.max
+
+fun main() {
+
+    fun parse(input: String): Pair<Int, Triple<Int, Int, Int>> {
+        var red = 0
+        var blue = 0
+        var green = 0
+        val (game, trials) = input.split(":", limit = 2)
+        val gameNoIx = game.lastIndexOf(" ")
+        val gameNo = game.substring(gameNoIx+1).toInt()
+        for (trial in trials.split(";")) {
+            val colorPairs = trial.split(",")
+            for (colorPair in colorPairs) {
+                val (num, color) = colorPair.trim().split("\\s+".toRegex())
+                when (color) {
+                    "red" -> red = max(red, num.toInt())
+                    "blue" -> blue = max(blue, num.toInt())
+                    "green" -> green = max(green, num.toInt())
+                }
+            }
+        }
+        return gameNo to Triple(red,green,blue)
+    }
+
+    fun <A> Triple<A,A,A>.all(predicate: (A) -> Boolean): Boolean {
+        return predicate(first) && predicate(second) && predicate(third)
+    }
+
+    infix fun Triple<Int, Int, Int>.zip(triple: Triple<Int, Int, Int>): Triple<Pair<Int, Int>, Pair<Int, Int>, Pair<Int, Int>> {
+        return Triple(first to triple.first, second to triple.second, third to triple.third)
+    }
+
+    infix fun Triple<Int, Int, Int>.lessThan(triple: Triple<Int, Int, Int>): Boolean {
+        return zip(triple).all { (a, b) -> a < b }
+    }
+
+    infix fun Triple<Int, Int, Int>.lessThanOrEqual(triple: Triple<Int, Int, Int>): Boolean {
+        return zip(triple).all { (a, b) -> a <= b }
+    }
+
+    fun part1(input: List<String>): Int {
+        var sum = 0
+        for (value in input) {
+            val (num, rgb) = parse(value)
+            if (rgb lessThanOrEqual Triple(12, 13, 14)) {
+//                num.println()
+                sum += num
+            }
+        }
+        return sum
+    }
+
+    fun Triple<Int,Int,Int>.power(): Int {
+        return first * second * third
+    }
+
+    fun part2(input: List<String>): Int {
+        var sum = 0
+        for (value in input) {
+            val (_, rgb) = parse(value)
+            val power = rgb.power()
+//            power.println()
+            sum += power
+        }
+        return sum
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    """.trimIndent().split("\n")
+
+    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
+
+//    val testInputTwo = """
+//        two1nine
+//        eightwothree
+//        abcone2threexyz
+//        xtwone3four
+//        4nineeightseven2
+//        zoneight234
+//        7pqrstsixteen
+//    """.trimIndent().split("\\s+".toRegex())
+//
+//    check(part2(testInputTwo) == 281)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
